d12e90ff2edb2fb33aa1fb18c0408e3570316b6c
Taylor Hedberg <t@tmh.cc>
2011-04-18 21:09:17 -0400

    Further reformatting

--- xmonad.hs
+++ xmonad.hs
@@ -19,22 +19,35 @@ import XMonad.Layout.WindowNavigation
 import XMonad.StackSet (focusDown)
 import XMonad.Util.EZConfig(additionalKeys)
 
+-- Shell commands
 cmd_browser = "firefox || ([ $? -eq 127 ] && chromium)"
 cmd_lockScreen = "slock"
 cmd_volDown = "amixer set Master 1- unmute"
 cmd_volUp = "amixer set Master 1+ unmute"
 cmd_volMute = "amixer set Master toggle"
 
+-- Key codes
 keyCode_volDown = 0x1008ff11
 keyCode_volUp = 0x1008ff13
 keyCode_volMute = 0x1008ff12
 
-color_focusedBorder = "#8f7100"
-color_normalBorder = "#111111"
+-- Color definitions
+colorDef_amber = "#dcae00"
+colorDef_darkAmber = "#8f7100"
+colorDef_midGray = "#111111"
 
+-- Color assignments
+color_focusedBorder = colorDef_darkAmber
+color_normalBorder = colorDef_midGray
+
+-- Misc constants
+my_terminal = "urxvt"
+my_modKey = mod4Mask
+
+-- General configuration
 myConfig = withUrgencyHook NoUrgencyHook defaultConfig 
-    { terminal = "urxvt" 
-    , modMask = mod4Mask
+    { terminal = my_terminal
+    , modMask = my_modKey
     , focusedBorderColor = color_focusedBorder
     , normalBorderColor = color_normalBorder
     , manageHook = floatNextHook
@@ -47,33 +60,37 @@ myConfig = withUrgencyHook NoUrgencyHook defaultConfig
         avoidStruts myLayouts
     } `additionalKeys` myKeys
 
-myKeys = [ ((mod4Mask .|. shiftMask, xK_l), spawn cmd_lockScreen)
+-- Custom key bindings
+myKeys = [ ((my_modKey .|. shiftMask, xK_l), spawn cmd_lockScreen)
          , ((0, keyCode_volDown), spawn cmd_volDown)
          , ((0, keyCode_volUp), spawn cmd_volUp)
          , ((0, keyCode_volMute), spawn cmd_volMute)
-         , ((mod4Mask, xK_i), withFocused (\f -> sendMessage (MinimizeWin f)))
-         , ((mod4Mask .|. shiftMask, xK_i), sendMessage RestoreNextMinimizedWin)
-         , ( (mod4Mask .|. controlMask, xK_i)
+         , ((my_modKey, xK_i), withFocused (\f -> sendMessage (MinimizeWin f)))
+         , ( (my_modKey .|. shiftMask, xK_i)
+           , sendMessage RestoreNextMinimizedWin
+           )
+         , ( (my_modKey .|. controlMask, xK_i)
            , withFocused (\f -> sendMessage (RestoreMinimizedWin f))
            )
-         , ( (mod4Mask, xK_backslash)
+         , ( (my_modKey, xK_backslash)
            , withFocused (sendMessage . maximizeRestore)
            )
-         , ((mod4Mask, xK_Up), sendMessage $ Go U)
-         , ((mod4Mask, xK_Down), sendMessage $ Go D)
-         , ((mod4Mask, xK_Left), sendMessage $ Go L)
-         , ((mod4Mask, xK_Right), sendMessage $ Go R)
-         , ((mod4Mask .|. shiftMask, xK_Up), sendMessage $ Swap U)
-         , ((mod4Mask .|. shiftMask, xK_Down), sendMessage $ Swap D)
-         , ((mod4Mask .|. shiftMask, xK_Left), sendMessage $ Swap L)
-         , ((mod4Mask .|. shiftMask, xK_Right), sendMessage $ Swap R)
-         , ((mod4Mask, xK_g), goToSelected defaultGSConfig)
-         , ((mod4Mask .|. shiftMask, xK_backslash), spawn cmd_browser)
-         , ((mod4Mask, xK_f), toggleFloatNext)
-         , ((mod4Mask, xK_d), toggleFloatAllNew)
+         , ((my_modKey, xK_Up), sendMessage $ Go U)
+         , ((my_modKey, xK_Down), sendMessage $ Go D)
+         , ((my_modKey, xK_Left), sendMessage $ Go L)
+         , ((my_modKey, xK_Right), sendMessage $ Go R)
+         , ((my_modKey .|. shiftMask, xK_Up), sendMessage $ Swap U)
+         , ((my_modKey .|. shiftMask, xK_Down), sendMessage $ Swap D)
+         , ((my_modKey .|. shiftMask, xK_Left), sendMessage $ Swap L)
+         , ((my_modKey .|. shiftMask, xK_Right), sendMessage $ Swap R)
+         , ((my_modKey, xK_g), goToSelected defaultGSConfig)
+         , ((my_modKey .|. shiftMask, xK_backslash), spawn cmd_browser)
+         , ((my_modKey, xK_f), toggleFloatNext)
+         , ((my_modKey, xK_d), toggleFloatAllNew)
          ]
 
-myStatusBar conf = statusBar ("dzen2 " ++ flags) dzenPP' toggleStrutsKey conf
+-- Status bar configuration
+myStatusBar = statusBar ("dzen2 " ++ flags) dzenPP' $ const (my_modKey, xK_b)
     where fg = "white"  -- Default: #a8a3f7
           bg = "black"  -- Default: #3f3c6d
           flags = "-e 'onstart=lower' -w 1055 -ta l -fg "
@@ -81,10 +98,8 @@ myStatusBar conf = statusBar ("dzen2 " ++ flags) dzenPP' toggleStrutsKey conf
                ++ " -bg "
                ++ bg
                ++ " -fn '-*-profont-*-*-*-*-11-*-*-*-*-*-*-*'"
-          dzenPP' = let amber = "#dcae00"
-                        darkamber = "#8f7100"
-                        lt = amber
-                        md = darkamber
+          dzenPP' = let lt = colorDef_amber
+                        md = colorDef_darkAmber
                         dk = "black"
                     in defaultPP
                         { ppCurrent = dzenColor dk lt . pad
@@ -101,15 +116,9 @@ myStatusBar conf = statusBar ("dzen2 " ++ flags) dzenPP' toggleStrutsKey conf
                         , ppTitle = dzenColor lt dk . pad . dzenEscape
                         }
 
-toggleStrutsKey XConfig{modMask = modm} = (modm, xK_b)
-
+-- Workspace layouts
 myLayouts = (maximize $ minimize $ Tall 1 (3/100) (1/2)) 
         ||| simpleTabbed 
-
-greekLCaseWorkspaces = ["α", "β", "γ", "δ", "ε", "ζ", "η", "θ", "ι"]
-greekUCaseWorkspaces =
-    [[chr (ord (head ch) - 0x20)] | ch <- greekLCaseWorkspaces]
-latinUCaseWorkspaces = ["A", "B", "C", "D", "E", "F", "G", "H", "I"]
 
 -- List of X11 window classes which should never steal focus
 noStealFocusWins = ["Pidgin"]
